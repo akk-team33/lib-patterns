cd7ca04e63e509212769ad9dcc11acfececa8529
Andi Kluge <andi.kluge@mobi4friends.de>
2016-04-26 13:09:43 +0200

    implementation and test: Collector

--- src/main/java/net/team33/patterns/Collector.java
+++ src/main/java/net/team33/patterns/Collector.java
@@ -1,6 +1,9 @@
 package net.team33.patterns;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class Collector<E, C extends Collection<E>> {
 
@@ -10,11 +13,101 @@ public class Collector<E, C extends Collection<E>> {
         this.subject = subject;
     }
 
-    public static <E, C extends Collection<E>> Collector<E, C> on(final C subject) {
+    public static <E, C extends Collection<E>> Collector<E, C> collect(final C subject) {
         return new Collector<>(subject);
     }
 
     public final C getSubject() {
         return subject;
+    }
+
+    public final Collector<E, C> add(final E element) {
+        subject.add(element);
+        return this;
+    }
+
+    public final Collector<E, C> add(final E element1, final E element2) {
+        return add(element1).add(element2);
+    }
+
+    public final Collector<E, C> add(final E element1, final E element2, final E element3) {
+        return add(element1).add(element2).add(element3);
+    }
+
+    @SuppressWarnings("OverloadedVarargsMethod")
+    @SafeVarargs
+    public final Collector<E, C> add(final E... elements) {
+        return addAll(Arrays.asList(elements));
+    }
+
+    public final Collector<E, C> addAll(final Collection<? extends E> elements) {
+        subject.addAll(elements);
+        return this;
+    }
+
+    public final Collector<E, C> addAll(final Iterable<? extends E> elements) {
+        return addAll(elements.iterator());
+    }
+
+    public final Collector<E, C> addAll(final Iterator<? extends E> elements) {
+        while (elements.hasNext()) {
+            add(elements.next());
+        }
+        return this;
+    }
+
+    public final Collector<E, C> remove(final E element) {
+        while (subject.remove(element)) {
+        }
+        return this;
+    }
+
+    public final Collector<E, C> remove(final E element1, final E element2) {
+        return remove(element1).remove(element2);
+    }
+
+    public final Collector<E, C> remove(final E element1, final E element2, final E element3) {
+        return remove(element1).remove(element2).remove(element3);
+    }
+
+    @SuppressWarnings("OverloadedVarargsMethod")
+    @SafeVarargs
+    public final Collector<E, C> remove(final E... elements) {
+        return removeAll(Arrays.asList(elements));
+    }
+
+    public final Collector<E, C> removeAll(final Collection<? extends E> elements) {
+        subject.removeAll(elements);
+        return this;
+    }
+
+    public final Collector<E, C> removeAll(final Iterable<? extends E> elements) {
+        return removeAll(elements.iterator());
+    }
+
+    public final Collector<E, C> removeAll(final Iterator<? extends E> elements) {
+        while (elements.hasNext()) {
+            remove(elements.next());
+        }
+        return this;
+    }
+
+    @SuppressWarnings("OverloadedVarargsMethod")
+    @SafeVarargs
+    public final Collector<E, C> retain(final E... elements) {
+        return retainAll(Arrays.asList(elements));
+    }
+
+    public final Collector<E, C> retainAll(final Collection<? extends E> elements) {
+        subject.retainAll(elements);
+        return this;
+    }
+
+    public final Collector<E, C> retainAll(final Iterable<? extends E> elements) {
+        return retainAll(elements.iterator());
+    }
+
+    public final Collector<E, C> retainAll(final Iterator<? extends E> elements) {
+        return removeAll(collect(new HashSet<E>(subject)).removeAll(elements).subject);
     }
 }
--- src/test/java/net/team33/patterns/CollectorTest.java
+++ src/test/java/net/team33/patterns/CollectorTest.java
@@ -1,13 +1,86 @@
 package net.team33.patterns;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.HashSet;
+import java.util.*;
 
 public class CollectorTest {
 
+    private static final List<Integer> INTEGERS = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
     @Test
-    public void test() {
-        final HashSet<Integer> subject = Collector.on(new HashSet<Integer>(0)).getSubject();
+    public final void add() {
+        final List<Integer> subject = Collector.collect(new ArrayList<Integer>(0))
+                .add(1)
+                .add(2, 3)
+                .add(4, 5, 6)
+                .add(7, 8, 9, 10)
+                .getSubject();
+        Assert.assertEquals(INTEGERS, subject);
+    }
+
+    @Test
+    public final void addAll() {
+        final List<Integer> subject = Collector.collect(new ArrayList<Integer>(0))
+                .addAll(Arrays.asList(1, 2, 3))
+                .addAll(new Integers(4, 5, 6))
+                .addAll(Arrays.asList(7, 8, 9, 10).iterator())
+                .getSubject();
+        Assert.assertEquals(INTEGERS, subject);
+    }
+
+    @Test
+    public final void remove() {
+        final List<Integer> subject = Collector.collect(new ArrayList<>(INTEGERS)).addAll(INTEGERS)
+                .remove(1)
+                .remove(2, 3)
+                .remove(4, 5, 6)
+                .remove(7, 8, 9, 10)
+                .getSubject();
+        Assert.assertEquals(Collections.emptyList(), subject);
+    }
+
+    @Test
+    public final void removeAll() {
+        final List<Integer> subject = Collector.collect(new ArrayList<>(INTEGERS)).addAll(INTEGERS)
+                .removeAll(Arrays.asList(1, 2, 3, 4, 5))
+                .removeAll(new Integers(4, 5, 6, 7, 8))
+                .removeAll(Arrays.asList(7, 8, 9, 10, 11).iterator())
+                .getSubject();
+        Assert.assertEquals(Collections.emptyList(), subject);
+    }
+
+    @Test
+    public final void retain() {
+        final List<Integer> subject = Collector.collect(new ArrayList<>(INTEGERS)).addAll(INTEGERS)
+                .retain(1, 2, 3, 4, 5)
+                .retain(4, 5, 6, 7, 8)
+                .retain(7, 8, 9, 10, 11)
+                .getSubject();
+        Assert.assertEquals(Collections.emptyList(), subject);
+    }
+
+    @Test
+    public final void retainAll() {
+        final List<Integer> subject = Collector.collect(new ArrayList<>(INTEGERS)).addAll(INTEGERS)
+                .retainAll(Arrays.asList(1, 2, 3, 4, 5))
+                .retainAll(new Integers(4, 5, 6, 7, 8))
+                .retainAll(Arrays.asList(7, 8, 9, 10, 11).iterator())
+                .getSubject();
+        Assert.assertEquals(Collections.emptyList(), subject);
+    }
+
+    private static class Integers implements Iterable<Integer> {
+        private final Collection<Integer> core;
+
+        private Integers(final Integer... values) {
+            core = Arrays.asList(values);
+        }
+
+        @Override
+        public final Iterator<Integer> iterator() {
+            return core.iterator();
+        }
     }
 }
