5594dc4a40980cbf3b13a170da723343288d9f62
KlugeKaindl <akk-gthb@team33.de>
2021-04-20 09:28:21 +0200

    refined Cases

--- exceptional-01/src/main/java/de/team33/patterns/exceptional/v1/Cases.java
+++ exceptional-01/src/main/java/de/team33/patterns/exceptional/v1/Cases.java
@@ -19,11 +19,11 @@ public class Cases<T extends Throwable> {
         return subject;
     }
 
-    public final <X> X quit(final Function<? super T, X> mapping) {
+    public final <R> R quit(final Function<? super T, R> mapping) {
         return mapping.apply(subject);
     }
 
-    public final <X extends Throwable> Cases<T> reThrow(final Class<X> xClass) throws X {
+    public final <X extends Throwable> Cases<T> reThrowIf(final Class<X> xClass) throws X {
         return throwIf(xClass::isInstance, xClass::cast);
     }
 
@@ -33,6 +33,59 @@ public class Cases<T extends Throwable> {
             throw mapping.apply(subject);
         } else {
             return this;
+        }
+    }
+
+    public final <TX extends Throwable> Conditional<T, TX> on(final Class<TX> txClass) {
+        return txClass.isInstance(subject) ? new Positive<>(txClass.cast(subject)) : new Negative<>(this);
+    }
+
+    public final Conditional<T, T> on(final Predicate<? super T> condition) {
+        return condition.test(subject) ? new Positive<>(subject) : new Negative<>(this);
+    }
+
+    public interface Conditional<T0 extends Throwable, TX extends Throwable> {
+
+        Cases<T0> reThrow() throws TX;
+
+        <X extends Throwable> Cases<T0> reThrow(Function<? super TX, X> mapping) throws X;
+    }
+
+    private static class Positive<T0 extends Throwable, TX extends Throwable> implements Conditional<T0, TX> {
+
+        private final TX subject;
+
+        private Positive(final TX subject) {
+            this.subject = subject;
+        }
+
+        @Override
+        public final Cases<T0> reThrow() throws TX {
+            throw subject;
+        }
+
+        @Override
+        public final <X extends Throwable> Cases<T0> reThrow(final Function<? super TX, X> mapping) throws X {
+            throw mapping.apply(subject);
+        }
+    }
+
+    private static class Negative<T0 extends Throwable, TX extends Throwable> implements Conditional<T0, TX> {
+
+        private final Cases<T0> cases;
+
+        private Negative(final Cases<T0> cases) {
+            this.cases = cases;
+        }
+
+        @Override
+        public final Cases<T0> reThrow() {
+            return cases;
+        }
+
+        @Override
+        public final <X extends Throwable> Cases<T0> reThrow(final Function<? super TX, X> mapping) {
+            return cases;
         }
     }
 }
--- exceptional-01/src/test/java/de/team33/patterns/exceptional/v1/CasesExample.java
+++ exceptional-01/src/test/java/de/team33/patterns/exceptional/v1/CasesExample.java
@@ -14,11 +14,11 @@ public class CasesExample {
             // We want to unwrap the cause of the caught exception and rethrow
             // it as a certain type of exception that meets our expectations ...
             throw Cases.of(caught.getCause())
-                       .reThrow(IOException.class)
-                       .reThrow(SQLException.class)
-                       .reThrow(URISyntaxException.class)
+                       .reThrowIf(IOException.class)
+                       .reThrowIf(SQLException.class)
+                       .reThrowIf(URISyntaxException.class)
                        .throwIf(Objects::isNull,
-                                cause -> new ExpectationException("WrappedException without cause!?", caught))
+                                any -> new ExpectationException("WrappedException without cause!?", caught))
                        // Technically, it could happen that our expectations are not met.
                        // To be on the safe side, this should lead to a meaningful exception ...
                        .quit(ExpectationException::new);
@@ -31,10 +31,27 @@ public class CasesExample {
         } catch (final WrappedException caught) {
             // We want to unwrap the cause of the caught exception and rethrow
             // it as a certain type of exception that meets our expectations ...
+            throw Cases.of(caught.getCause())
+                       .on(IOException.class).reThrow()
+                       .on(SQLException.class).reThrow()
+                       .on(URISyntaxException.class).reThrow()
+                       .on(Objects::isNull).reThrow(any -> new ExpectationException("WrappedException without cause!?", caught))
+                       // Technically, it could happen that our expectations are not met.
+                       // To be on the safe side, this should lead to a meaningful exception ...
+                       .quit(ExpectationException::new);
+        }
+    }
+
+    public void sample3() throws IOException, SQLException, URISyntaxException {
+        try {
+            doSomethingThatMayThrowAWrappedException();
+        } catch (final WrappedException caught) {
+            // We want to unwrap the cause of the caught exception and rethrow
+            // it as a certain type of exception that meets our expectations ...
             final Throwable cause = Cases.of(caught.getCause())
-                                         .reThrow(IOException.class)
-                                         .reThrow(SQLException.class)
-                                         .reThrow(URISyntaxException.class)
+                                         .reThrowIf(IOException.class)
+                                         .reThrowIf(SQLException.class)
+                                         .reThrowIf(URISyntaxException.class)
                                          .quit();
             // Technically, it could happen that our expectations are not met.
             // To be on the safe side, this should lead to a meaningful exception ...
