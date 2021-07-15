59d92b2263d02a4df58fd1945a3d583528ab1ba8
KlugeKaindl <akk-gthb@team33.de>
2021-07-15 09:12:13 +0200

    lazy-01: slight refactoring and updated javadoc

--- lazy-01/src/main/java/de/team33/patterns/lazy/v1/XLazy.java
+++ lazy-01/src/main/java/de/team33/patterns/lazy/v1/XLazy.java
@@ -3,11 +3,12 @@ package de.team33.patterns.lazy.v1;
 import de.team33.patterns.exceptional.v1.XSupplier;
 
 /**
- * <p>Implements an {@link XSupplier} that provides a virtually fixed value.
- * This value is only actually determined when it is accessed for the first time.</p>
- *
- * <p>This implementation ensures that the {@linkplain #XLazy(XSupplier) originally defined initialization code}
- * is called at most once, even when there is concurrent access from multiple threads.</p>
+ * Implements a {@link XSupplier} that provides a virtually fixed value.
+ * This value is only actually determined when it is accessed for the first time.
+ * <p>
+ * This implementation ensures that the {@linkplain #XLazy(XSupplier) originally defined initialization code} is called
+ * at most once, even if there is concurrent access from multiple threads, unless the initialization attempt causes an
+ * exception.
  */
 public class XLazy<T, X extends Exception> extends LazyBase<T, X> implements XSupplier<T, X> {
 
@@ -18,11 +19,6 @@ public class XLazy<T, X extends Exception> extends LazyBase<T, X> implements XSu
         super(initial);
     }
 
-    /**
-     * <p>Returns the represented value.</p>
-     * <p>That value was determined once with the first access using the
-     * {@linkplain #XLazy(XSupplier) originally defined initialization code}.</p>
-     */
     @Override
     public final T get() throws X {
         return backing.get();
