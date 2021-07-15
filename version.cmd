380667bc43346911ced883024178279efab02850
KlugeKaindl <akk-gthb@team33.de>
2021-07-15 09:41:14 +0200

    added version scripts

---
+++ version.cmd
@@ -0,0 +1,20 @@
+@ECHO OFF
+IF "%1" == "" GOTO usage
+IF NOT "%2" == "" GOTO usage
+
+SET TAG=%1
+
+echo You are using the tag: %TAG%
+pause
+
+CALL mvn versions:set -DnewVersion=%TAG% -DgenerateBackupPoms=false
+GOTO end
+
+:usage
+ECHO.
+ECHO usage: %0 VERSION_TAG
+ECHO example (I): %0 3.0.5-SNAPSHOT
+ECHO example (II): %0 4.0
+ECHO.
+
+:end
---
+++ version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if (($# != 1)); then
+  echo "usage: $0 VERSION_TAG"
+  echo "example (I): $0 3.0.5-SNAPSHOT"
+  echo "example (II): $0 4.0"
+  exit
+fi
+
+TAG=$1
+
+echo "You are using the tag: $TAG"
+read -p "Press enter to continue"
+
+mvn versions:set -DnewVersion=$TAG -DgenerateBackupPoms=false
