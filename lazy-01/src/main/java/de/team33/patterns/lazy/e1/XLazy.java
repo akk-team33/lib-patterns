c8e2e2c4dd829f63838a297e68d1f2baaa878ae6
KlugeKaindl <akk-gthb@team33.de>
2021-08-09 10:11:37 +0200

    Use main version for all modules instead of module specific versions + slight enhancements in lazy-01

--- lazy-01/src/main/java/de/team33/patterns/lazy/e1/XLazy.java
+++ lazy-01/src/main/java/de/team33/patterns/lazy/e1/XLazy.java
@@ -3,17 +3,18 @@ package de.team33.patterns.lazy.e1;
 import de.team33.patterns.exceptional.e1.XSupplier;
 
 /**
- * Implements a {@link XSupplier} that provides a virtually fixed value.
+ * Implements an {@link XSupplier} that provides a virtually fixed value.
  * This value is only actually determined when it is accessed for the first time.
  * <p>
- * This implementation ensures that the {@linkplain #XLazy(XSupplier) originally defined initialization code} is called
- * at most once, even if there is concurrent access from multiple threads, unless the initialization attempt causes an
- * exception.
+ * This implementation ensures that the {@linkplain #XLazy(XSupplier) originally defined initialization code}
+ * is called at most once, even if there is concurrent access from multiple threads, unless the
+ * initialization attempt causes an exception.
  */
 public class XLazy<T, X extends Exception> extends LazyBase<T, X> implements XSupplier<T, X> {
 
     /**
-     * Initializes a new instance giving a supplier that defines the intended initialization of the represented value.
+     * Initializes a new instance giving a supplier that defines the intended initialization of the
+     * represented value.
      */
     public XLazy(final XSupplier<T, X> initial) {
         super(initial);
