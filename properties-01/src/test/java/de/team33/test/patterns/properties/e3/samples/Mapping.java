d71588c1df17c34ae6631fc199843178d1d8e460
andi <akk-pg@team33.de>
2021-10-13 13:53:11 +0200

    added e3.Basics

---
+++ properties-01/src/test/java/de/team33/test/patterns/properties/e3/samples/Mapping.java
@@ -0,0 +1,15 @@
+package de.team33.test.patterns.properties.e3.samples;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Mapping {
+
+    static Map<String, Object> toMap(final BasicsSample sample) {
+        final TreeMap<String, Object> result = new TreeMap<>();
+        result.put("intProp", sample.getIntProp());
+        result.put("strProp", sample.getStrProp());
+        result.put("listProp", sample.getListProp());
+        return result;
+    }
+}
