2acea7ac38dcf5ee03cb2c4d5460203924175b12
KlugeKaindl <akk-gthb@team33.de>
2021-10-22 22:18:21 +0200

    Added e5/Fields and some tests

---
+++ properties-01/src/test/java/de/team33/test/patterns/properties/e5/ReMappingTest.java
@@ -0,0 +1,72 @@
+package de.team33.test.patterns.properties.e5;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Map;
+import java.util.Random;
+import java.util.function.Supplier;
+
+import de.team33.patterns.pooling.e1.Provider;
+import de.team33.patterns.properties.e5.BiMapping;
+import de.team33.patterns.properties.e5.Fields;
+import de.team33.patterns.properties.e5.ReMapping;
+import de.team33.test.patterns.properties.shared.AnyClass;
+import de.team33.test.patterns.properties.shared.MapMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+
+class ReMappingTest
+{
+
+    private static final Provider<Random> RANDOM = new Provider<>(Random::new);
+
+    @ParameterizedTest
+    @EnumSource(Case.class)
+    final void map(final Case testCase)
+    {
+        // prepare ...
+        final ReMapping<AnyClass> mapping = testCase.mapping.get();
+        final AnyClass expected = RANDOM.get(AnyClass::new);
+        final Map<String, Object> origin = expected.toMap(MapMode.DEEP);
+
+        // charge ...
+        final AnyClass result = mapping.map(origin).to(new AnyClass());
+
+        // verify ...
+        assertEquals(expected, result);
+    }
+
+    enum Case
+    {
+
+        RE_MAPPING_DECLARATIVE(() -> ReMapping.add("aLong", AnyClass::setALong)
+                                              .add("aBigDecimal", AnyClass::setABigDecimal)
+                                              .add("aList", AnyClass::setAList)
+                                              .add("anInt", AnyClass::setAnInt)
+                                              .add("aDouble", AnyClass::setADouble)
+                                              .add("aString", AnyClass::setAString)
+                                              .add("aDate", AnyClass::setADate)
+                                              .build()),
+
+        BI_MAPPING_DECLARATIVE(() -> BiMapping.add("aLong", AnyClass::getALong, AnyClass::setALong)
+                                              .add("aBigDecimal",
+                                                   AnyClass::getABigDecimal,
+                                                   AnyClass::setABigDecimal)
+                                              .add("aList", AnyClass::getAList, AnyClass::setAList)
+                                              .add("anInt", AnyClass::getAnInt, AnyClass::setAnInt)
+                                              .add("aDouble", AnyClass::getADouble, AnyClass::setADouble)
+                                              .add("aString", AnyClass::getAString, AnyClass::setAString)
+                                              .add("aDate", AnyClass::getADate, AnyClass::setADate)
+                                              .build()),
+
+        BI_MAPPING_BY_FIELDS(() -> Fields.mapping(AnyClass.class, Fields.Mode.DEEP));
+
+        final Supplier<ReMapping<AnyClass>> mapping;
+
+        Case(final Supplier<ReMapping<AnyClass>> mapping)
+        {
+            this.mapping = mapping;
+        }
+    }
+}
