cc6fa403ee9e27f74b15adbc97bbb4792805361b
Andreas Kluge-Kaindl <akk-gthb@team33.de>
2021-12-14 14:04:15 +0100

    lazy-01: migrated to junit-jupiter & used testing-01

--- lazy-01/src/test/java/de/team33/test/patterns/lazy/e1/LazyTest.java
+++ lazy-01/src/test/java/de/team33/test/patterns/lazy/e1/LazyTest.java
@@ -1,55 +1,56 @@
 package de.team33.test.patterns.lazy.e1;
 
 import de.team33.patterns.lazy.e1.Lazy;
-import org.junit.Test;
+import de.team33.patterns.testing.e1.Parallel;
+import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 
-import static de.team33.test.patterns.lazy.e1.Runner.parallel;
-import static de.team33.test.patterns.lazy.e1.Runner.sequential;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class LazyTest {
+class LazyTest {
 
     private static final String ZERO_SMALLER_FIRST =
             "Although <lazy> already exists, the date <zero> must be smaller than the (first) evaluation of <lazy>";
 
     private int counter = 0;
-    private Lazy<Date> lazy = new Lazy<>(() -> {
+    private final Lazy<Date> lazy = new Lazy<>(() -> {
         try {
             counter += 1;
             Thread.sleep(1);
             return new Date();
         } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
             throw new IllegalStateException(e.getMessage(), e);
         }
     });
 
     @Test
-    public final void getFirst() throws InterruptedException {
+    final void getFirst() throws InterruptedException {
         final Date zero = new Date();
         Thread.sleep(1);
-        assertTrue(ZERO_SMALLER_FIRST, zero.compareTo(lazy.get()) < 0);
+        assertTrue(zero.compareTo(lazy.get()) < 0, ZERO_SMALLER_FIRST);
     }
 
     @Test
-    public final void getSame() {
-        assertSame("If <lazy> is evaluated several times, the result must always be the same.", lazy.get(), lazy.get());
+    final void getSame() {
+        assertSame(lazy.get(), lazy.get(),
+                   "If <lazy> is evaluated several times, the result must always be the same.");
     }
 
     @Test
-    public final void getSequential() {
+    final void getSequential() throws InterruptedException {
         assertEquals(0, counter);
-        sequential(100, () -> lazy.get());
+        Parallel.apply(100, 1, ignored -> lazy.get());
         assertEquals(1, counter);
     }
 
     @Test
-    public final void getParallel() {
+    final void getParallel() throws InterruptedException {
         assertEquals(0, counter);
-        parallel(100, () -> lazy.get());
+        Parallel.apply(100, ignored -> lazy.get());
         assertEquals(1, counter);
     }
 }
