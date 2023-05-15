b420fd58db675637025a218b956ef1104390b9cb
andi <akk-pg@team33.de>
2023-05-15 11:16:45 +0200

    [serving-naiad] refined Aspect

--- patterns-lib/serving-naiad/src/main/java/de/team33/patterns/serving/naiad/Aspect.java
+++ patterns-lib/serving-naiad/src/main/java/de/team33/patterns/serving/naiad/Aspect.java
@@ -1,13 +1,45 @@
 package de.team33.patterns.serving.naiad;
 
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 /**
- * Abstracts an <em>"aspect"</em>: a kind of property that can be useful as part of an interactive application's
- * backend service. While a property in the common sense merely represents a value of a certain type
- * and in particular can be readable and/or modifiable, an <em>"aspect"</em> is additionally able to provide an
- * interface to notify an audience about changes of its value.
+ * Abstracts an <em>"aspect"</em> as a component of a service.
+ * More precisely: a kind of property that can be useful as part of an interactive application's business service.
+ * While a property in the common sense simply represents a value of a certain type and, in particular,
+ * may be readable and/or modifiable, an <em>"aspect"</em> is additionally able to provide an interface to notify
+ * an audience of changes in its content.
+ * Also, unlike a normal property, an <em>"aspect"</em> has no value semantics, even if the underlying type has
+ * value semantics. In other words: two aspects with different identities are never
+ * {@link Object#equals(Object) equal}, even if their contained values are {@link Object#equals(Object) equal}.
+ * Finally, an <em>"aspect"</em> is not to be viewed as a representation of a value,
+ * but rather as a container for a value.
  *
- * @param <T> The type of the provided value.
+ * @param <T> The type of the contained value.
  */
 public interface Aspect<T> {
 
+    /**
+     * Abstracts a readable {@link Aspect}.
+     *
+     * @param <T> The type of the contained value.
+     */
+    @FunctionalInterface
+    interface Readable<T> extends Supplier<T>, Aspect<T> {
+    }
+
+    /**
+     * Abstracts a modifiable {@link Aspect}.
+     *
+     * @param <T> The type of the contained value.
+     */
+    @FunctionalInterface
+    interface Modifiable<T> extends Consumer<T>, Aspect<T> {
+    }
+
+    @FunctionalInterface
+    interface Connectable<T> extends Aspect<T> {
+
+        void connect(Consumer<T> listener);
+    }
 }
