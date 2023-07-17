46effeda658f0192f27099068c7b18bf1b945416
Kluge-Kaindl, Andreas <andreas.klugekaindl@governikus.de>
2023-07-17 15:20:31 +0200

    [pooling-e1] renamed "subject" to "item"

--- patterns-lib/pooling-01/src/main/java/de/team33/patterns/pooling/e1/RProvider.java
+++ patterns-lib/pooling-01/src/main/java/de/team33/patterns/pooling/e1/RProvider.java
@@ -10,31 +10,31 @@ import java.util.function.Supplier;
 
 /**
  * A tool that makes instances of a certain type available for the course of operations that require such an instance.
- * The instances provided are referred to as <em>subject</em> in the following.
+ * The instances provided are referred to as <em>item</em> in the following.
  * <p>
- * The tool maintains a number of such <em>subjects</em> and only creates as many as are actually required at most at
- * the same time in its application context. The <em>subjects</em> are retained and reused for subsequent operations.
+ * The tool maintains a number of such <em>items</em> and only creates as many as are actually required at most at
+ * the same time in its application context. The <em>items</em> are retained and reused for subsequent operations.
  * <p>
- * In this respect, this tool is suitable for providing <em>subject</em> types whose instantiation is relatively
+ * In this respect, this tool is suitable for providing <em>item</em> types whose instantiation is relatively
  * "expensive", which are rather unsuitable for concurrent access, but are designed for multiple or permanent use.
  * Database or other client-server connections may be an example.
  * <p>
  * Note: this implementation cannot detect when an internal operation is taking place in the course of an operation to
- * which the same <em>subject</em> could be made available.
+ * which the same <em>item</em> could be made available.
  * <p>
- * This implementation does not support checked exceptions to occur while creating new <em>subject</em> instances.
+ * This implementation does not support checked exceptions to occur while creating new <em>item</em> instances.
  *
- * @param <S> The type of provided instances <em>(subjects)</em>.
+ * @param <I> The type of provided instances <em>(items)</em>.
  * @see XProvider
  */
-public class RProvider<S> extends Mutual<Recent<S>, RuntimeException> {
+public class RProvider<I> extends Mutual<Recent<I>, RuntimeException> {
 
     /**
      * Initializes a new instance giving a {@link Supplier} that defines the intended initialization of a
-     * new <em>subject</em>.
+     * new <em>item</em>.
      */
-    public RProvider(final Supplier<S> newItem, final long maxIdle, final long maxLiving) {
-        super(() -> new Recent<S>(newItem, maxIdle, maxLiving));
+    public RProvider(final Supplier<I> newItem, final long maxIdle, final long maxLiving) {
+        super(() -> new Recent<I>(newItem, maxIdle, maxLiving));
     }
 
     /**
@@ -43,7 +43,7 @@ public class RProvider<S> extends Mutual<Recent<S>, RuntimeException> {
      * While the {@link Consumer} is being executed, the parameter is exclusively available to it, but must not be
      * "hijacked" from the context of the execution or the executing thread!
      */
-    public final void run(final Consumer<? super S> consumer) {
+    public final void run(final Consumer<? super I> consumer) {
         accept(recent -> consumer.accept(recent.get()));
     }
 
@@ -56,7 +56,7 @@ public class RProvider<S> extends Mutual<Recent<S>, RuntimeException> {
      * @param <X> A type of exception that may be caused by the given {@link XConsumer}.
      * @throws X if the execution of the given {@link XConsumer} causes one.
      */
-    public final <X extends Exception> void runEx(final XConsumer<? super S, X> xConsumer) throws X {
+    public final <X extends Exception> void runEx(final XConsumer<? super I, X> xConsumer) throws X {
         accept(recent -> xConsumer.accept(recent.get()));
     }
 
@@ -69,7 +69,7 @@ public class RProvider<S> extends Mutual<Recent<S>, RuntimeException> {
      *
      * @param <R> The result type of the given {@link Function}
      */
-    public final <R> R get(final Function<? super S, R> function) {
+    public final <R> R get(final Function<? super I, R> function) {
         return apply(recent -> function.apply(recent.get()));
     }
 
@@ -84,7 +84,7 @@ public class RProvider<S> extends Mutual<Recent<S>, RuntimeException> {
      * @param <X> A type of exception that may be caused by the given {@link XFunction}.
      * @throws X if the execution of the given {@link XFunction} causes one.
      */
-    public final <R, X extends Exception> R getEx(final XFunction<? super S, R, X> xFunction) throws X {
+    public final <R, X extends Exception> R getEx(final XFunction<? super I, R, X> xFunction) throws X {
         return apply(recent -> xFunction.apply(recent.get()));
     }
 }
--- patterns-lib/pooling-01/src/main/java/de/team33/patterns/pooling/e1/XProvider.java
+++ patterns-lib/pooling-01/src/main/java/de/team33/patterns/pooling/e1/XProvider.java
@@ -9,31 +9,31 @@ import java.util.function.Function;
 
 /**
  * A tool that makes instances of a certain type available for the course of operations that require such an instance.
- * The instances provided are referred to as <em>subject</em> in the following.
+ * The instances provided are referred to as <em>item</em> in the following.
  * <p>
- * The tool maintains a number of such <em>subjects</em> and only creates as many as are actually required at most at
- * the same time in its application context. The <em>subjects</em> are retained and reused for subsequent operations.
+ * The tool maintains a number of such <em>items</em> and only creates as many as are actually required at most at
+ * the same time in its application context. The <em>items</em> are retained and reused for subsequent operations.
  * <p>
- * In this respect, this tool is suitable for providing <em>subject</em> types whose instantiation is relatively
+ * In this respect, this tool is suitable for providing <em>item</em> types whose instantiation is relatively
  * "expensive", which are rather unsuitable for concurrent access, but are designed for multiple or permanent use.
  * Database or other client-server connections may be an example.
  * <p>
  * Note: this implementation cannot detect when an internal operation is taking place in the course of an operation to
- * which the same <em>subject</em> could be made available.
+ * which the same <em>item</em> could be made available.
  * <p>
- * This implementation supports checked exceptions to occur while creating new <em>subject</em> instances.
+ * This implementation supports checked exceptions to occur while creating new <em>item</em> instances.
  *
- * @param <S> The type of provided instances <em>(subjects)</em>
- * @param <E> A type of exception that may be caused by the creation of new <em>subject</em> instances.
+ * @param <I> The type of provided instances <em>(items)</em>
+ * @param <E> A type of exception that may be caused by the creation of new <em>item</em> instances.
  * @see Provider
  */
-public class XProvider<S, E extends Exception> extends Mutual<S, E> {
+public class XProvider<I, E extends Exception> extends Mutual<I, E> {
 
     /**
      * Initializes a new instance giving an {@link XSupplier} that defines the intended initialization of a
-     * new <em>subject</em>.
+     * new <em>item</em>.
      */
-    public XProvider(final XSupplier<S, E> newItem) {
+    public XProvider(final XSupplier<I, E> newItem) {
         super(newItem);
     }
 
@@ -43,9 +43,9 @@ public class XProvider<S, E extends Exception> extends Mutual<S, E> {
      * While the {@link Consumer} is being executed, the parameter is exclusively available to it, but must not be
      * "hijacked" from the context of the execution or the executing thread!
      *
-     * @throws E if the initialization of a new <em>subject</em> causes one.
+     * @throws E if the initialization of a new <em>item</em> causes one.
      */
-    public final void run(final Consumer<? super S> consumer) throws E {
+    public final void run(final Consumer<? super I> consumer) throws E {
         accept(consumer::accept);
     }
 
@@ -56,10 +56,10 @@ public class XProvider<S, E extends Exception> extends Mutual<S, E> {
      * "hijacked" from the context of the execution or the executing thread!
      *
      * @param <X> A type of exception that may be caused by the given {@link XConsumer}.
-     * @throws E if the initialization of a new <em>subject</em> causes one.
+     * @throws E if the initialization of a new <em>item</em> causes one.
      * @throws X if the execution of the given {@link XConsumer} causes one.
      */
-    public final <X extends Exception> void runEx(final XConsumer<? super S, X> xConsumer) throws E, X {
+    public final <X extends Exception> void runEx(final XConsumer<? super I, X> xConsumer) throws E, X {
         accept(xConsumer);
     }
 
@@ -71,9 +71,9 @@ public class XProvider<S, E extends Exception> extends Mutual<S, E> {
      * "hijacked" from the context of the call or the executing thread!
      *
      * @param <R> The result type of the given {@link Function}
-     * @throws E if the initialization of a new <em>subject</em> causes one.
+     * @throws E if the initialization of a new <em>item</em> causes one.
      */
-    public final <R> R get(final Function<? super S, R> function) throws E {
+    public final <R> R get(final Function<? super I, R> function) throws E {
         return apply(function::apply);
     }
 
@@ -86,10 +86,10 @@ public class XProvider<S, E extends Exception> extends Mutual<S, E> {
      *
      * @param <R> The result type of the given {@link XFunction}
      * @param <X> A type of exception that may be caused by the given {@link XFunction}.
-     * @throws E if the initialization of a new <em>subject</em> causes one.
+     * @throws E if the initialization of a new <em>item</em> causes one.
      * @throws X if the execution of the given {@link XFunction} causes one.
      */
-    public final <R, X extends Exception> R getEx(final XFunction<? super S, R, X> xFunction) throws E, X {
+    public final <R, X extends Exception> R getEx(final XFunction<? super I, R, X> xFunction) throws E, X {
         return apply(xFunction);
     }
 }
