fbadffca2e3bf7c99c49fab8b285d00025cb8d58
Kluge-Kaindl, Andreas <akk-pg@team33.de>
2023-08-19 14:15:28 +0200

    removed deprecated modules

--- patterns-lib/exceptional-01/src/main/java/de/team33/patterns/exceptional/e1/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * @deprecated Further development is discontinued and this package/module may be removed in a future release.
- * Successor is the module <em>exceptional-dione</em>.
- */
-package de.team33.patterns.exceptional.e1;
