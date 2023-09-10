5b5779aac4c52aee6ecc6f435155929e70a7e145
Andreas Kluge-Kaindl <akk-pg@team33.de>
2023-09-10 20:07:09 +0200

    [serial-charon] refined tests

--- patterns-lib/serial-charon/src/test/java/de/team33/patterns/serial/charon/ChargedTest.java
+++ patterns-lib/serial-charon/src/test/java/de/team33/patterns/serial/charon/ChargedTest.java
@@ -30,4 +30,69 @@ class ChargedTest {
             assertInstanceOf(Empty.class, subject);
         }
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8})
+    final void isEmpty(final int index) {
+        final Series<?> subject = Charged.seriesOf(values, index);
+        assertEquals(index >= values.size(), subject.isEmpty());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8})
+    final void isCharged(final int index) {
+        final Series<?> subject = Charged.seriesOf(values, index);
+        assertEquals(index < values.size(), subject.isCharged());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8})
+    final void ifCharged(final int index) {
+        final Series<?> subject = Charged.seriesOf(values, index);
+        assertEquals(subject instanceof Charged, subject.ifCharged((head, tail) -> head).isPresent());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8})
+    final void head(final int index) {
+        final Series<?> subject = Charged.seriesOf(values, index);
+        if (subject instanceof Charged) {
+            assertEquals(index, subject.head());
+        } else {
+            assertInstanceOf(Empty.class, subject);
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8})
+    final void tail(final int index) {
+        final Series<?> subject = Charged.seriesOf(values, index);
+        if (subject instanceof Charged) {
+            assertEquals(Charged.seriesOf(values, index + 1), subject.tail());
+        } else {
+            assertInstanceOf(Empty.class, subject);
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8})
+    final void size(final int index) {
+        final Series<?> subject = Charged.seriesOf(values, index);
+        if (subject instanceof Charged) {
+            assertEquals(values.size() - index, subject.size());
+        } else {
+            assertInstanceOf(Empty.class, subject);
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8})
+    final void asList(final int index) {
+        final Series<?> subject = Charged.seriesOf(values, index);
+        if (subject instanceof Charged) {
+            assertEquals(values.subList(index, values.size()), subject.asList());
+        } else {
+            assertInstanceOf(Empty.class, subject);
+        }
+    }
 }
