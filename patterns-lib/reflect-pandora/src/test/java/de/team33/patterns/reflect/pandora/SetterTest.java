45aeb2e7506cbb62a01e9b05a9cd7020e36ea0b1
Kluge-Kaindl, Andreas <andreas.klugekaindl@governikus.de>
2023-10-29 13:29:29 +0100

    [reflect-pandora] refined tests

---
+++ patterns-lib/reflect-pandora/src/test/java/de/team33/patterns/reflect/pandora/SetterTest.java
@@ -0,0 +1,49 @@
+package de.team33.patterns.reflect.pandora;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetterTest {
+
+    @Test
+    void accept_IllegalAccessException() throws NoSuchMethodException {
+        final String name = "ensureCapacityInternal";
+        final Setter<ArrayList<?>> setter = new Setter<>(ArrayList.class.getDeclaredMethod(name, int.class));
+        final ArrayList<Object> target = new ArrayList<>();
+
+        try {
+            setter.accept(target, 25);
+            fail("expected to fail - but was " + target);
+        } catch (final IllegalStateException e) {
+            // as expected!
+            // e.printStackTrace();
+            assertInstanceOf(IllegalAccessException.class, e.getCause());
+            assertTrue(e.getMessage().contains(name));
+        }
+    }
+
+    @Test
+    void accept_InvocationTargetException() throws NoSuchMethodException {
+        final String name = "setLeadsToInvocationTargetException";
+        final Setter<SetterTest> setter = new Setter<>(getClass().getDeclaredMethod(name, int.class));
+
+        try {
+            setter.accept(this, 25);
+            fail("expected to fail - but was " + this);
+        } catch (final IllegalStateException e) {
+            // as expected!
+            e.printStackTrace();
+            assertInstanceOf(InvocationTargetException.class, e.getCause());
+            assertTrue(e.getMessage().contains(name));
+        }
+    }
+
+    final void setLeadsToInvocationTargetException(final int value) {
+        throw new UnsupportedOperationException("should lead to an InvocationTargetException");
+    }
+}
