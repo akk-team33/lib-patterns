a1f34ee05e2f415e5c2ede582f6f5bc8a428682d
Andreas Kluge-Kaindl <akk-pg@team33.de>
2024-02-20 14:31:05 +0100

    [generics-atlas] Renamed TypedefPackageTest -> TypedefExtraTest

--- patterns-lib/generics-atlas/src/test/java/de/team33/patterns/generics/atlas/TypedefExtraTest.java
+++ patterns-lib/generics-atlas/src/test/java/de/team33/patterns/generics/atlas/TypedefExtraTest.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TypedefPackageTest {
+class TypedefExtraTest {
 
     @Test
     void getActualParameter_unknown() {
