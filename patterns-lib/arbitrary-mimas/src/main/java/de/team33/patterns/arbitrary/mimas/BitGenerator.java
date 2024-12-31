0d5ac46d748158ab1b1143a529924d99674e23b8
Andreas Kluge-Kaindl <akk-pg@team33.de>
2024-12-31 12:50:57 +0100

    [hashing-pandia] refined javadoc

--- patterns-lib/arbitrary-mimas/src/main/java/de/team33/patterns/arbitrary/mimas/BitGenerator.java
+++ patterns-lib/arbitrary-mimas/src/main/java/de/team33/patterns/arbitrary/mimas/BitGenerator.java
@@ -5,11 +5,13 @@ import java.math.BigInteger;
 interface BitGenerator {
 
     /**
-     * Returns any non-negative {@link BigInteger} representing a sequence of significant bits of a given length.
-     * In other words, the result is any value between zero (inclusive) and 2<sup>length</sup> (exclusive).
+     * Returns any non-negative {@link BigInteger} that represents a sequence of a specified number of significant bits.
+     * In other words, the result is any value between zero (inclusive) and 2<sup><em>numBits</em></sup> (exclusive).
      * <p>
      * A typical implementation will return an arbitrary value within the defined bounds, with each possible value
      * being equally probable.
+     *
+     * @param numBits The intended number of significant bits.
      */
     BigInteger anyBits(final int numBits);
 }
