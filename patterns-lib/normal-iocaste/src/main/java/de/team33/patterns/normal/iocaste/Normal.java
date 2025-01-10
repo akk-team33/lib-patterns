11431288177f98cf15b47aeaad6d7aa1a9df73e0
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-01-10 18:12:34 +0100

    [normal-iocaste] some drafts

--- patterns-lib/normal-iocaste/src/main/java/de/team33/patterns/normal/iocaste/Normal.java
+++ patterns-lib/normal-iocaste/src/main/java/de/team33/patterns/normal/iocaste/Normal.java
@@ -1,4 +1,24 @@
 package de.team33.patterns.normal.iocaste;
 
+/**
+ * Represents a normalized data object.
+ */
 public abstract class Normal {
+
+    /**
+     * Returns the {@link Type} of <em>this</em> instance.
+     */
+    public abstract Type type();
+
+    /**
+     * Represents the type of {@link Normal normalized data object}.
+     */
+    public enum Type {
+
+        SIMPLE,
+
+        SEQUENCE,
+
+        COMPOUND;
+    }
 }
