c48dffa33b8a33b2afb2455d900f9459663114c2
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-01-12 15:24:55 +0100

    [normal-iocaste] some drafts (step 4)

--- patterns-lib/normal-iocaste/src/main/java/de/team33/patterns/normal/iocaste/Value.java
+++ patterns-lib/normal-iocaste/src/main/java/de/team33/patterns/normal/iocaste/Value.java
@@ -4,4 +4,25 @@ package de.team33.patterns.normal.iocaste;
  * Represents a normalized value object.
  */
 public abstract class Value {
+
+    /**
+     * Symbolizes the type of {@link Value value object}.
+     */
+    public enum Type {
+
+        /**
+         * Symbolizes an elementary value object.
+         */
+        ELEMENT,
+
+        /**
+         * Symbolizes a value object consisting of multiple values.
+         */
+        SEQUENCE,
+
+        /**
+         * Symbolizes a value object consisting of key-value pairs.
+         */
+        COMPOUND;
+    }
 }
--- patterns-lib/normal-iocaste/src/main/java/de/team33/patterns/normal/iocaste/ValueList.java
+++ patterns-lib/normal-iocaste/src/main/java/de/team33/patterns/normal/iocaste/ValueList.java
@@ -1,7 +1,7 @@
 package de.team33.patterns.normal.iocaste;
 
 /**
- * Represents a normalized value object that consists of multiple values.
+ * Represents a normalized value object consisting of multiple values.
  */
 public class ValueList extends Value {
 }
