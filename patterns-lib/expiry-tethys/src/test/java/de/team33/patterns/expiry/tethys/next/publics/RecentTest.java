ced958bd4ddaf0b5591fd2f64bea1166070f5b56
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-09-16 20:16:17 +0200

    refined RecentTest

--- patterns-lib/expiry-tethys/src/test/java/de/team33/patterns/expiry/tethys/next/publics/RecentTest.java
+++ patterns-lib/expiry-tethys/src/test/java/de/team33/patterns/expiry/tethys/next/publics/RecentTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -15,33 +14,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class RecentTest {
 
     private static final System.Logger LOG = System.getLogger(RecentTest.class.getCanonicalName());
-    private static final AtomicInteger ATOMIC_INTEGER = new AtomicInteger(0);
+    private static final long MAX_IDLE = 3;
+    private static final long MAX_LIVING = 20;
 
-    private final Meter meter = new Meter();
-    private final Supplier<Long> initial = meter::next;
-    private final long maxIdle = 3;
-    private final long maxLiving = 20;
-    private final Recent<Long> recent = new Recent<>(initial, maxIdle, maxLiving);
-
-    @Test
-    final void test_maxLiving() throws InterruptedException {
-        final List<Long> result = collect(recent, 100, 2);
-        LOG.log(System.Logger.Level.INFO, result);
-        for (int i = 1; i < result.size(); ++i) {
-            final long delta = result.get(i) - result.get(i - 1);
-            assertTrue(delta >= maxLiving);
-        }
-    }
-
-    @Test
-    final void test_maxIdle() throws InterruptedException {
-        final List<Long> result = collect(recent, 100, 4);
-        LOG.log(System.Logger.Level.INFO, result);
-        for (int i = 1; i < result.size(); ++i) {
-            final long delta = result.get(i) - result.get(i - 1);
-            assertTrue(delta >= maxIdle);
-        }
-    }
+    private final Lazy<Long> time0 = Lazy.init(System::currentTimeMillis);
+    private final Supplier<Long> initial = () -> System.currentTimeMillis() - time0.get();
+    private final Recent<Long> recent = new Recent<>(initial, MAX_IDLE, MAX_LIVING);
 
     private static <T extends Comparable<T>>
     List<T> collect(final Recent<T> recent, final int count, final long time) throws InterruptedException {
@@ -53,12 +31,23 @@ class RecentTest {
         return List.copyOf(result);
     }
 
-    private static class Meter {
+    @Test
+    final void test_maxLiving() throws InterruptedException {
+        final List<Long> result = collect(recent, 100, MAX_IDLE - 1);
+        LOG.log(System.Logger.Level.INFO, result);
+        for (int i = 1; i < result.size(); ++i) {
+            final long delta = result.get(i) - result.get(i - 1);
+            assertTrue(delta >= MAX_LIVING);
+        }
+    }
 
-        private final Lazy<Long> time0 = Lazy.init(System::currentTimeMillis);
-
-        final long next() {
-            return System.currentTimeMillis() - time0.get();
+    @Test
+    final void test_maxIdle() throws InterruptedException {
+        final List<Long> result = collect(recent, 100, MAX_IDLE + 1);
+        LOG.log(System.Logger.Level.INFO, result);
+        for (int index = 1; index < result.size(); ++index) {
+            final long delta = result.get(index) - result.get(index - 1);
+            assertTrue(delta >= MAX_IDLE);
         }
     }
 }
