58fdf444751a1017789f4782260dc3e577a1b8c7
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-09-20 20:38:44 +0200

    refined RecentTest & use bridging-styx

--- patterns-lib/expiry-tethys/src/test/java/de/team33/patterns/expiry/tethys/next/publics/RecentTest.java
+++ patterns-lib/expiry-tethys/src/test/java/de/team33/patterns/expiry/tethys/next/publics/RecentTest.java
@@ -2,6 +2,7 @@ package de.team33.patterns.expiry.tethys.next.publics;
 
 import de.team33.patterns.expiry.tethys.Recent;
 import de.team33.patterns.lazy.narvi.Lazy;
+import de.team33.testing.bridging.styx.Bridger;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -11,29 +12,29 @@ import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class RecentTest {
+class RecentTest extends Bridger {
 
     private static final System.Logger LOG = System.getLogger(RecentTest.class.getCanonicalName());
-    private static final long MAX_IDLE = 3;
-    private static final long MAX_LIVING = 20;
+    private static final int MAX_IDLE = 5;
+    private static final int MAX_LIVING = 20;
 
     private final Lazy<Long> time0 = Lazy.init(System::currentTimeMillis);
     private final Supplier<Long> initial = () -> System.currentTimeMillis() - time0.get();
     private final Recent<Long> recent = new Recent<>(initial, MAX_IDLE, MAX_LIVING);
 
-    private static <T extends Comparable<T>>
-    List<T> collect(final Recent<T> recent, final int count, final long time) throws InterruptedException {
+    private <T extends Comparable<T>>
+    List<T> collect(final Recent<T> recent, final int count, final int time) throws InterruptedException {
         final Set<T> result = new TreeSet<>();
         for (int index = 0; index < count; ++index) {
             result.add(recent.get());
-            Thread.sleep(time);
+            bridge(time);
         }
         return List.copyOf(result);
     }
 
     @Test
-    final void test_maxLiving() throws InterruptedException {
-        final List<Long> result = collect(recent, 100, MAX_IDLE - 1);
+    final void get_maxLiving() throws InterruptedException {
+        final List<Long> result = collect(recent, 100, MAX_IDLE / 2);
         LOG.log(System.Logger.Level.INFO, result);
         for (int i = 1; i < result.size(); ++i) {
             final long delta = result.get(i) - result.get(i - 1);
@@ -42,8 +43,8 @@ class RecentTest {
     }
 
     @Test
-    final void test_maxIdle() throws InterruptedException {
-        final List<Long> result = collect(recent, 100, MAX_IDLE + 1);
+    final void get_maxIdle() throws InterruptedException {
+        final List<Long> result = collect(recent, 100, MAX_IDLE * 2);
         LOG.log(System.Logger.Level.INFO, result);
         for (int index = 1; index < result.size(); ++index) {
             final long delta = result.get(index) - result.get(index - 1);
