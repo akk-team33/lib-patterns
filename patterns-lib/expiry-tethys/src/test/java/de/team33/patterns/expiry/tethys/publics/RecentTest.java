94dfed81b918b6bd2480a88795a0b42ca6eebb74
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-09-21 20:36:06 +0200

    [lazy-narvy] better test coverage

--- patterns-lib/expiry-tethys/src/test/java/de/team33/patterns/expiry/tethys/publics/RecentTest.java
+++ patterns-lib/expiry-tethys/src/test/java/de/team33/patterns/expiry/tethys/publics/RecentTest.java
@@ -1,11 +1,25 @@
 package de.team33.patterns.expiry.tethys.publics;
 
+import de.team33.patterns.expiry.tethys.Recent;
 import de.team33.testing.bridging.styx.Bridger;
 import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 class RecentTest extends Bridger {
 
     @Test
     final void get() {
+        final Recent<Instant> recent = new Recent<>(Instant::now, 100, 1000);
+        final Instant first = recent.get();
+        bridge(50);
+        assertSame(first, recent.get());
+        bridge(50);
+        assertSame(first, recent.get());
+        bridge(150);
+        assertNotEquals(first, recent.get());
     }
 }
--- patterns-lib/expiry-tethys/src/test/java/de/team33/patterns/expiry/tethys/publics/XRecentTest.java
+++ patterns-lib/expiry-tethys/src/test/java/de/team33/patterns/expiry/tethys/publics/XRecentTest.java
@@ -1,11 +1,25 @@
 package de.team33.patterns.expiry.tethys.publics;
 
+import de.team33.patterns.expiry.tethys.XRecent;
 import de.team33.testing.bridging.styx.Bridger;
 import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 class XRecentTest extends Bridger {
 
     @Test
-    final void get() {
+    final void get() throws Exception {
+        final XRecent<Instant, Exception> recent = new XRecent<>(Instant::now, 100, 1000);
+        final Instant first = recent.get();
+        bridge(50);
+        assertSame(first, recent.get());
+        bridge(50);
+        assertSame(first, recent.get());
+        bridge(150);
+        assertNotEquals(first, recent.get());
     }
 }
