cd712fa565a9807c852c156835fc6fb1b983f53f
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-09-30 20:55:36 +0200

    [stream-galatea] added Aggregate, enhanced Streamable

--- patterns-lib/stream-galatea/src/main/java/de/team33/patterns/stream/galatea/Streamable.java
+++ patterns-lib/stream-galatea/src/main/java/de/team33/patterns/stream/galatea/Streamable.java
@@ -1,9 +1,65 @@
 package de.team33.patterns.stream.galatea;
 
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 @FunctionalInterface
 public interface Streamable<E> {
 
+    static <E> Streamable<E> empty() {
+        return Stream::empty;
+    }
+
+    static <E> Streamable<E> of(final E element) {
+        return () -> Stream.of(element);
+    }
+
+    @SafeVarargs
+    static <E> Streamable<E> of(final E element0, final E element1, final E ... more) {
+        return () -> Stream.concat(Stream.of(element0, element1), Stream.of(more));
+    }
+
+    /**
+     * Returns a sequential {@code Stream} with <em>this</em> streamable as its source.
+     * <p>
+     * An implementation may or may not specify a streaming order.
+     */
     Stream<E> stream();
+
+    /**
+     * Returns {@code true} if <em>this</em> streamable contains at least one <em>element</em>
+     * such that <em>condition</em>{@link Predicate#test(Object) .test(element)} is {@code true}.
+     */
+    default boolean containsAny(final Predicate<? super E> condition) {
+        return stream().anyMatch(condition);
+    }
+
+    /**
+     * Returns {@code true} if <em>this</em> streamable contains at least one <em>element</em>
+     * such that {@code Objects.equals(element, candidate)}.
+     */
+    default boolean contains(final Object candidate) {
+        return containsAny(element -> Objects.equals(element, candidate));
+    }
+
+    /**
+     * Returns {@code true} if <em>this</em> streamable contains all the specified <em>candidates</em>.
+     */
+    default boolean containsAll(final Streamable<?> candidates) {
+        return candidates.stream().allMatch(this::contains);
+    }
+
+    /**
+     * Performs the given <em>action</em> for each element of <em>this</em> streamable
+     * until all elements have been processed or the action throws an exception.
+     * Actions are performed in the streaming order, if that order is specified.
+     * Exceptions thrown by the action are relayed to the caller.
+     *
+     * @throws NullPointerException if the specified <em>action</em> is {@code null}.
+     */
+    default void forEach(final Consumer<? super E> action) {
+        stream().forEach(action);
+    }
 }
--- patterns-lib/stream-galatea/src/test/java/de/team33/patterns/stream/galatea/publics/StreamableTest.java
+++ patterns-lib/stream-galatea/src/test/java/de/team33/patterns/stream/galatea/publics/StreamableTest.java
@@ -1,19 +1,78 @@
 package de.team33.patterns.stream.galatea.publics;
 
+import de.team33.patterns.arbitrary.mimas.Generator;
 import de.team33.patterns.stream.galatea.Streamable;
 import org.junit.jupiter.api.Test;
 
+import java.security.SecureRandom;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static java.util.function.Predicate.not;
+import static org.junit.jupiter.api.Assertions.*;
 
 class StreamableTest {
 
+    private static final Generator GENERATOR = Generator.of(new SecureRandom());
+
+    private final List<String> origin = Stream.generate(GENERATOR::anyString)
+                                              .limit(GENERATOR.anyInt(4, 8))
+                                              .toList();
+
     @Test
     final void stream() {
-        final List<String> expected = List.of("a", "b", "c");
-        final Streamable<String> streamable = expected::stream;
-        final List<String> result = streamable.stream().toList();
-        assertEquals(expected, result);
+        final Streamable<String> streamable = origin::stream;
+        assertEquals(origin, streamable.stream().toList());
+    }
+
+    @Test
+    final void empty() {
+        assertTrue(Streamable.empty().stream().findAny().isEmpty());
+    }
+
+    @Test
+    final void of_one() {
+        final Streamable<String> streamable = Streamable.of(origin.get(0));
+        assertEquals(List.of(origin.get(0)), streamable.stream().toList());
+    }
+
+    @Test
+    final void of_two() {
+        final Streamable<String> streamable = Streamable.of(origin.get(0), origin.get(1));
+        assertEquals(List.of(origin.get(0), origin.get(1)), streamable.stream().toList());
+    }
+
+    @Test
+    final void of_more() {
+        final Streamable<String> streamable = Streamable.of(origin.get(0), origin.get(1), origin.get(2));
+        assertEquals(List.of(origin.get(0), origin.get(1), origin.get(2)), streamable.stream().toList());
+    }
+
+    @Test
+    final void containsAny() {
+        final Streamable<String> streamable = origin::stream;
+        assertTrue(streamable.containsAny(origin::contains));
+        assertFalse(streamable.containsAny(not(origin::contains)));
+    }
+
+    @Test
+    final void contains() {
+        final Streamable<String> streamable = origin::stream;
+        assertTrue(streamable.contains(origin.get(1)));
+    }
+
+    @Test
+    final void containsAll() {
+        final Streamable<String> streamable = origin::stream;
+        assertTrue(streamable.containsAll(Streamable.of(origin.get(0), origin.get(2), origin.get(3))));
+    }
+
+    @Test
+    final void forEach() {
+        final List<String> result = new LinkedList<>();
+        final Streamable<String> streamable = origin::stream;
+        streamable.forEach(result::add);
+        assertEquals(origin, result);
     }
 }
