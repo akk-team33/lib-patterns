dc8f89ddf348517ed86e84f44d3306a7f7cd2a7f
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-10-15 23:24:14 +0200

    [proving-kerberos] added method Guard.proved + test

--- patterns-lib/proving-kerberos/src/main/java/de/team33/patterns/proving/kerberos/Guard.java
+++ patterns-lib/proving-kerberos/src/main/java/de/team33/patterns/proving/kerberos/Guard.java
@@ -1,12 +1,22 @@
 package de.team33.patterns.proving.kerberos;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 @SuppressWarnings("BooleanParameter")
-public final class Guard {
+public final class Guard<I, E extends Exception> {
 
-    private Guard() {
+    private final Predicate<I> condition;
+    private final Function<I, String> toMessage;
+    private final Function<String, E> toException;
+
+    private Guard(final Predicate<I> condition,
+                  final Function<I, String> toMessage,
+                  final Function<String, E> toException) {
+        this.condition = condition;
+        this.toMessage = toMessage;
+        this.toException = toException;
     }
 
     /**
@@ -34,5 +44,20 @@ public final class Guard {
      */
     public static void prove(final boolean condition) {
         prove(condition, () -> "prove failed");
+    }
+
+    /**
+     * Returns a {@link Guard} that can prove that an input of type {@code <I>} meets a given <em>condition</em>
+     * or throws an {@link IllegalArgumentException} with a message supplied by <em>toMessage</em>
+     * if the input does not satisfy that <em>condition</em>.
+     */
+    public static <I> Guard<I, IllegalArgumentException> proving(final Predicate<I> condition,
+                                                                 final Function<I, String> toMessage) {
+        return new Guard<>(condition, toMessage, IllegalArgumentException::new);
+    }
+
+    public <K extends I> K proved(final K input) throws E {
+        prove(condition.test(input), () -> toMessage.apply(input), toException);
+        return input;
     }
 }
--- patterns-lib/proving-kerberos/src/test/java/de/team33/patterns/proving/kerberos/publics/GuardTest.java
+++ patterns-lib/proving-kerberos/src/test/java/de/team33/patterns/proving/kerberos/publics/GuardTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.io.IOException;
 import java.util.UUID;
 
-import static java.lang.System.Logger.Level.INFO;
+import static java.lang.System.Logger.Level.DEBUG;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GuardTest {
@@ -15,6 +15,7 @@ class GuardTest {
     private static final System.Logger LOGGER =
             System.getLogger(GuardTest.class.getCanonicalName());
 
+    @SuppressWarnings("ConstantValue")
     @ParameterizedTest
     @ValueSource(booleans = {false, true})
     final void prove_defaultMessage(final boolean condition) {
@@ -22,12 +23,13 @@ class GuardTest {
             Guard.prove(condition);
             assertTrue(condition, () -> "<condition> is expected to be true - but was %s".formatted(condition));
         } catch (final IllegalStateException e) {
-            LOGGER.log(INFO, e::getMessage, e);
+            LOGGER.log(DEBUG, e::getMessage, e);
             assertFalse(condition, () -> "<condition> is expected to be false - but was %s".formatted(condition));
             assertEquals("prove failed", e.getMessage());
         }
     }
 
+    @SuppressWarnings("ConstantValue")
     @ParameterizedTest
     @ValueSource(booleans = {false, true})
     final void prove(final boolean condition) {
@@ -36,12 +38,13 @@ class GuardTest {
             Guard.prove(condition, () -> message);
             assertTrue(condition, () -> "<condition> is expected to be true - but was %s".formatted(condition));
         } catch (final IllegalStateException e) {
-            LOGGER.log(INFO, e::getMessage, e);
+            LOGGER.log(DEBUG, e::getMessage, e);
             assertFalse(condition, () -> "<condition> is expected to be false - but was %s".formatted(condition));
             assertEquals(message, e.getMessage());
         }
     }
 
+    @SuppressWarnings("ConstantValue")
     @ParameterizedTest
     @ValueSource(booleans = {false, true})
     final void prove_IOException(final boolean condition) {
@@ -50,9 +53,28 @@ class GuardTest {
             Guard.prove(condition, () -> message, IOException::new);
             assertTrue(condition, () -> "<condition> is expected to be true - but was %s".formatted(condition));
         } catch (final IOException e) {
-            LOGGER.log(INFO, e::getMessage, e);
+            LOGGER.log(DEBUG, e::getMessage, e);
             assertFalse(condition, () -> "<condition> is expected to be false - but was %s".formatted(condition));
             assertEquals(message, e.getMessage());
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-5, -3, -1, 1, 3, 5})
+    final void proved(final int given) {
+        final Guard<Integer, IllegalArgumentException> guard =
+                Guard.proving(input -> 0 < input,
+                              "<input> is expected to be greater than 0 - but was %d"::formatted);
+        try {
+            final int result = guard.proved(given);
+            assertEquals(given, result);
+            assertTrue(0 < given, () -> ("<given> is expected to be greater than 0 " +
+                                         "- but was %s").formatted(given));
+        } catch (final IllegalArgumentException e) {
+            LOGGER.log(DEBUG, e::getMessage, e);
+            assertFalse(0 < given, () -> ("<given> is expected to be less than or equal to 0 " +
+                                          "- but was %s").formatted(given));
+            assertEquals("<input> is expected to be greater than 0 - but was %d".formatted(given), e.getMessage());
         }
     }
 }
