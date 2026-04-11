6613ef001268e835e990bca7e021c9bb346277eb
Andi Kluge <akk-dev@team33.de>
2026-04-11 12:09:09 +0200

    [1.x] Resume module serial-charon

---
+++ patterns-lib/serial-charon/src/main/java/de/team33/patterns/serial/charon/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides an alternative abstraction of Collections
+ * whose access methods are suitable for both recursive and successive serial processing.
+ *
+ * @see <a href="https://de.wikipedia.org/wiki/Charon_(Mond)">Charon</a>
+ */
+package de.team33.patterns.serial.charon;
