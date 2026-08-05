d763d54180a5a7e84585c3ce90048542dcea44ed
Andi Kluge <akk-dev@team33.de>
2026-08-05 08:50:50 +0200

    [2.x] Nailed module proving-kerberos to 2.7.0 (suspended)

--- patterns-lib/proving-kerberos/src/main/java/de/team33/patterns/proving/kerberos/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides proving states through guard clauses.
- *
- * @see <a href="https://de.wikipedia.org/wiki/Kerberos_(Mond)" target="_blank">Kerberos (Mond)</a>
- */
-package de.team33.patterns.proving.kerberos;
