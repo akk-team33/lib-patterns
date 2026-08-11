d910c9f1f8fa789c29350a777e083657bcb183a2
Andi Kluge <akk-dev@team33.de>
2026-08-11 15:49:41 +0200

    [2.x] Pinned module hierarchy-mab to 2.8.0 (deprecated)

--- patterns-lib/hierarchy-mab/src/main/java/de/team33/patterns/hierarchy/mab/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * <b>Deprecated</b>: This module is basically just an implementation core for the deprecated module
- * <a href="https://www.team33.de/dev/patterns/2.x/patterns-lib/io-adrastea/apidocs/">io-adrastea</a>
- * and is largely useless on its own.
- *
- * @see <a href="https://www.team33.de/dev/patterns/2.x/patterns-lib/io-adrastea/">io-adrastea</a>
- * @see <a href="https://www.team33.de/dev/patterns/2.x/patterns-lib/io-adrastea/apidocs/">io-adrastea/apidocs</a>
- */
-package de.team33.patterns.hierarchy.mab;
