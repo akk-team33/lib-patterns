963f29bb4894b1dbdbdf416c5fc38264fd209d22
Andi Kluge <akk-dev@team33.de>
2026-08-11 16:40:45 +0200

    [2.x] Added building.* to legacy-testing

---
+++ patterns-annex/legacy-testing/src/test/java/de/team33/patterns/building/elara/publics/ProtoBuilderTest.java
@@ -0,0 +1,46 @@
+package de.team33.patterns.building.elara.publics;
+
+import de.team33.patterns.building.elara.ProtoBuilder;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ProtoBuilderTest {
+
+    private static final ProtoBuilder.Lifecycle LIFECYCLE = new ProtoBuilder.Lifecycle() {
+        @Override
+        public void check() {
+            // nothing to do here
+        }
+
+        @Override
+        public void increment() {
+            // nothing to do here
+        }
+    };
+
+    @Test
+    final void legal() {
+        assertInstanceOf(LegalBuilder.class, new LegalBuilder());
+    }
+
+    @Test
+    final void illegal() {
+        assertThrows(IllegalArgumentException.class, IllegalBuilder::new);
+    }
+
+    static class LegalBuilder extends ProtoBuilder<StringBuilder, LegalBuilder> {
+
+        LegalBuilder() {
+            super(new StringBuilder(0), LIFECYCLE, LegalBuilder.class);
+        }
+    }
+
+    static class IllegalBuilder extends ProtoBuilder<StringBuilder, LegalBuilder> {
+
+        IllegalBuilder() {
+            super(new StringBuilder(0), LIFECYCLE, LegalBuilder.class);
+        }
+    }
+}
