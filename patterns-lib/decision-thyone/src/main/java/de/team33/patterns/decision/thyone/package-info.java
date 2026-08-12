80494ba6d028248abb58e496e500b035ee2e6d3c
Andi Kluge <akk-dev@team33.de>
2026-08-12 15:30:05 +0200

    [2.x] Pinned module decision-thyone to 2.8.1 (still featured)

--- patterns-lib/decision-thyone/src/main/java/de/team33/patterns/decision/thyone/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides classes and utilities for implementing or handling complex decisions.
- *
- * @see <a href="https://de.wikipedia.org/wiki/Thyone_(Mond)" target="_blank">Thyone (Mond)</a>
- */
-package de.team33.patterns.decision.thyone;
