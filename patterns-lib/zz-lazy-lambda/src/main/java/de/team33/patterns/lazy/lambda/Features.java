87d37248ea9fad854556a608ee3a85d01f4868fd
Andi Kluge <akk-dev@team33.de>
2026-08-18 09:20:31 +0200

    [2.x-lazy-lambda] refined Features, added Sample

--- patterns-lib/zz-lazy-lambda/src/main/java/de/team33/patterns/lazy/lambda/Features.java
+++ patterns-lib/zz-lazy-lambda/src/main/java/de/team33/patterns/lazy/lambda/Features.java
@@ -1,21 +1,29 @@
 package de.team33.patterns.lazy.lambda;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 
 public class Features {
 
     @SuppressWarnings("rawtypes")
-    private final Map<Key, Lazy> cache = new ConcurrentHashMap<>();
+    private final Map<Key, Lazy> backing = new ConcurrentHashMap<>();
 
     @SuppressWarnings("unchecked")
-    private <T> Lazy<T> lazy(final Key<T> key, final Supplier<T> supplier) {
-        return cache.computeIfAbsent(key, any -> Lazy.init(supplier));
+    public final <T> T get(final Key<T> key, final Supplier<T> supplier) {
+        return (T) backing.computeIfAbsent(key, any -> Lazy.init(supplier))
+                          .get();
     }
 
-    public final <T> T get(final Key<T> key, final Supplier<T> supplier) {
-        return lazy(key, supplier).get();
+    @SuppressWarnings("unchecked")
+    public final <T> Optional<T> peek(final Key<T> key) {
+        return (Optional<T>) Optional.ofNullable(backing.get(key))
+                                     .map(Lazy::get);
+    }
+
+    public final void reset() {
+        backing.clear();
     }
 
     @SuppressWarnings("MarkerInterface")
