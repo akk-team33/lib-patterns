c429c20072d86a6cb7203d54c2987afa1dfa0d69
Andi Kluge <akk-dev@team33.de>
2026-08-22 07:02:24 +0200

    [2.x-records-triton] simplified Generalizer

--- patterns-lib/records-triton/src/main/java/de/team33/patterns/records/triton/Generalizer.java
+++ patterns-lib/records-triton/src/main/java/de/team33/patterns/records/triton/Generalizer.java
@@ -6,87 +6,107 @@ import java.lang.reflect.Array;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.function.Predicate;
 
-@SuppressWarnings("MethodMayBeStatic")
-final class Generalizer {
+enum Generalizer {
 
-    private final Mapping mapping;
+    BOOLEAN(Boolean.class, Generalizer::mapBoolean),
+    BYTE(Byte.class, Generalizer::mapByte),
+    SHORT(Short.class, Generalizer::mapShort),
+    INT(Integer.class, Generalizer::mapInt),
+    LONG(Long.class, Generalizer::mapLong),
+    FLOAT(Float.class, Generalizer::mapFloat),
+    DOUBLE(Double.class, Generalizer::mapDouble),
+    BIG_INT(BigInteger.class, Generalizer::mapBigInteger),
+    BIG_DEC(BigDecimal.class, Generalizer::mapBigDecimal),
+    CHAR(Character.class, Generalizer::mapChar),
+    STRING(String.class, Generalizer::mapString),
+    ENUM(Enum.class, Generalizer::mapEnum),
+    ARRAY(Class::isArray, Generalizer::mapArray),
+    RECORD(Record.class, Generalizer::mapRecord),
+    STRINGABLE(Stringable::supports, Generalizer::mapStringable);
 
-    private Generalizer(final Class<?> sourceClass) {
-        this.mapping = Mapping.of(sourceClass);
-        if (null == mapping) {
-            throw new IllegalArgumentException("cannot map " + sourceClass.getCanonicalName());
-        }
+    private static final Values<Generalizer> VALUES = Values.of(Generalizer.class);
+
+    private final Predicate<Class<?>> responsibility;
+    @SuppressWarnings("rawtypes")
+    private final Mapper mapper;
+
+    <T> Generalizer(final Class<T> normalClass, final Mapper<T> mapper) {
+        this(normalClass::isAssignableFrom, mapper);
+    }
+
+    <T> Generalizer(final Predicate<Class<?>> responsibility, final Mapper<T> mapper) {
+        this.responsibility = responsibility;
+        this.mapper = mapper;
     }
 
     static JsonValue map(final Object source) {
         if (null == source) {
             return JsonValue.NULL;
         } else {
-            return new Generalizer(source.getClass()).apply(source);
+            final Class<?> sourceClass = source.getClass();
+            return VALUES.findAny(value -> value.responsibility.test(sourceClass))
+                         .map(value -> value.mapper)
+                         .map(mapper -> mapper.map(source))
+                         .orElseThrow(() -> new IllegalArgumentException(
+                                 "cannot map " + sourceClass.getCanonicalName()));
         }
     }
 
-    private JsonValue apply(final Object source) {
-        return mapping.method(this)
-                      .apply(source);
-    }
-
-    private JsonBoolean mapBoolean(final boolean source) {
+    private static JsonBoolean mapBoolean(final boolean source) {
         return new JsonBoolean(source);
     }
 
-    private JsonNumber mapByte(final byte source) {
+    private static JsonNumber mapByte(final byte source) {
         return mapLong(source);
     }
 
-    private JsonNumber mapShort(final short source) {
+    private static JsonNumber mapShort(final short source) {
         return mapLong(source);
     }
 
-    private JsonNumber mapInt(final int source) {
+    private static JsonNumber mapInt(final int source) {
         return mapLong(source);
     }
 
-    private JsonNumber mapLong(final long source) {
+    private static JsonNumber mapLong(final long source) {
         return new JsonNumber(BigDecimal.valueOf(source));
     }
 
-    private JsonNumber mapFloat(final float source) {
+    private static JsonNumber mapFloat(final float source) {
         return new JsonNumber(new BigDecimal(Float.toString(source)));
     }
 
-    private JsonNumber mapDouble(final double source) {
+    private static JsonNumber mapDouble(final double source) {
         return new JsonNumber(new BigDecimal(Double.toString(source)));
     }
 
-    private JsonNumber mapBigInteger(final BigInteger source) {
+    private static JsonNumber mapBigInteger(final BigInteger source) {
         return new JsonNumber(new BigDecimal(source));
     }
 
-    private JsonNumber mapBigDecimal(final BigDecimal source) {
+    private static JsonNumber mapBigDecimal(final BigDecimal source) {
         return new JsonNumber(source);
     }
 
-    private JsonValue mapEnum(final Enum<?> source) {
+    private static JsonValue mapEnum(final Enum<?> source) {
         return new JsonString(source.name());
     }
 
-    private JsonString mapChar(final char source) {
+    private static JsonString mapChar(final char source) {
         return new JsonString(Character.toString(source));
     }
 
-    private JsonString mapString(final String source) {
+    private static JsonString mapString(final String source) {
         return new JsonString(source);
     }
 
-    private JsonString mapStringable(final Object source) {
+    private static JsonString mapStringable(final Object source) {
         return new JsonString(Stringable.encode(source));
     }
 
-    private JsonArray mapArray(final Object array) {
+    private static JsonArray mapArray(final Object array) {
         final JsonArray.Builder builder = JsonArray.builder();
         final int length = Array.getLength(array);
         for (int index = 0; index < length; ++index) {
@@ -96,7 +116,7 @@ final class Generalizer {
         return builder.build();
     }
 
-    private JsonObject mapRecord(final Record source) {
+    private static JsonObject mapRecord(final Record source) {
         final var builder = JsonObject.builder();
         final var stage = Triton.toMap(source);
         for (final Map.Entry<String, Object> entry : stage.entrySet()) {
@@ -105,57 +125,14 @@ final class Generalizer {
         return builder.build();
     }
 
-    private enum Mapping {
+    @FunctionalInterface
+    interface Mapper<T> {
 
-        BOOLEAN(Boolean.class, Generalizer::mapBoolean),
-        BYTE(Byte.class, Generalizer::mapByte),
-        SHORT(Short.class, Generalizer::mapShort),
-        INT(Integer.class, Generalizer::mapInt),
-        LONG(Long.class, Generalizer::mapLong),
-        FLOAT(Float.class, Generalizer::mapFloat),
-        DOUBLE(Double.class, Generalizer::mapDouble),
-        BIG_INT(BigInteger.class, Generalizer::mapBigInteger),
-        BIG_DEC(BigDecimal.class, Generalizer::mapBigDecimal),
-        CHAR(Character.class, Generalizer::mapChar),
-        STRING(String.class, Generalizer::mapString),
-        ENUM(Enum.class, Generalizer::mapEnum),
-        ARRAY(Class::isArray, Generalizer::mapArray),
-        RECORD(Record.class, Generalizer::mapRecord),
-        STRINGABLE(Stringable::supports, Generalizer::mapStringable);
+        JsonValue mapT(T source);
 
-        private static final Values<Mapping> VALUES = Values.of(Mapping.class);
-
-        private final Predicate<Class<?>> responsibility;
-        @SuppressWarnings("rawtypes")
-        private final Method method;
-
-        <T> Mapping(final Class<T> normalClass, final Method<T> method) {
-            this(normalClass::isAssignableFrom, method);
-        }
-
-        <T> Mapping(final Predicate<Class<?>> responsibility, final Method<T> method) {
-            this.responsibility = responsibility;
-            this.method = method;
-        }
-
-        static Mapping of(final Class<?> sourceClass) {
-            return VALUES.findAny(value -> value.responsibility.test(sourceClass))
-                         .orElse(null);
-        }
-
-        final Function<Object, JsonValue> method(final Generalizer mapper) {
-            return source -> method.map(mapper, source);
-        }
-
-        @FunctionalInterface
-        interface Method<T> {
-
-            JsonValue mapT(Generalizer mapper, T source);
-
-            @SuppressWarnings("unchecked")
-            default JsonValue map(final Generalizer mapper, final Object source) {
-                return mapT(mapper, (T) source);
-            }
+        @SuppressWarnings("unchecked")
+        default JsonValue map(final Object source) {
+            return mapT((T) source);
         }
     }
 }
