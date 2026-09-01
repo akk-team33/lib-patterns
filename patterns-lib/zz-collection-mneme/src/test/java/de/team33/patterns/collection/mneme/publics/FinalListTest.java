26ae72a3446f35fc71c715fa3ae2dbe0ef72c326
Andi Kluge <akk-dev@team33.de>
2026-09-01 07:52:13 +0200

    [2.x] zz-collection-mneme: refined tests

--- patterns-lib/zz-collection-mneme/src/test/java/de/team33/patterns/collection/mneme/publics/FinalListTest.java
+++ patterns-lib/zz-collection-mneme/src/test/java/de/team33/patterns/collection/mneme/publics/FinalListTest.java
@@ -2,16 +2,17 @@ package de.team33.patterns.collection.mneme.publics;
 
 import de.team33.patterns.arbitrary.mimas.Generator;
 import de.team33.patterns.collection.mneme.FinalList;
-import de.team33.patterns.streamable.naiad.Streamer;
+import de.team33.patterns.streamable.naiad.Streamable;
 import org.junit.jupiter.api.Test;
 
 import java.security.SecureRandom;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class FinalListTest {
 
@@ -19,47 +20,87 @@ class FinalListTest {
 
     @Test
     final void empty() {
-        assertTrue(FinalList.empty().isEmpty());
+        assertEquals(List.of(), FinalList.empty());
     }
 
     @Test
-    final void tryBuild_map_reduce() {
-        final List<Integer> expected = Stream.generate(() -> GENERATOR.anyInt(0, 10))
-                                             .limit(20000)
-                                             .toList();
-        final FinalList<Integer> result = expected.stream()
-                                                  .map(Streamer::of)
-                                                  .reduce(Streamer.empty(), Streamer::addAll)
-                                                  .map(FinalList::of);
+    final void of_single() {
+        final List<String> expected = Stream.generate(GENERATOR::anyString).limit(1).toList();
+        final FinalList<String> result = FinalList.of(expected.get(0));
         assertEquals(expected, result);
     }
 
     @Test
-    final void tryBuild_collect() {
-        final List<Integer> expected = Stream.generate(() -> GENERATOR.anyInt(0, 10))
-                                             .limit(20000)
-                                             .toList();
-        final FinalList<Integer> result = expected.stream()
-                                                  .collect(Streamer::<Integer>empty, Streamer::add, Streamer::addAll)
-                                                  .map(FinalList::of);
+    final void of_two() {
+        final List<String> expected = Stream.generate(GENERATOR::anyString).limit(2).toList();
+        final FinalList<String> result = FinalList.of(expected.get(0), expected.get(1));
+        assertEquals(expected, result);
+    }
+
+    @Test
+    final void of_three() {
+        final List<String> expected = Stream.generate(GENERATOR::anyString).limit(3).toList();
+        final FinalList<String> result = FinalList.of(expected.get(0), expected.get(1), expected.get(2));
+        assertEquals(expected, result);
+    }
+
+    @Test
+    final void of_more() {
+        final List<String> expected = Stream.generate(GENERATOR::anyString).limit(5).toList();
+        final FinalList<String> result = FinalList.of(expected.get(0),
+                                                      expected.get(1),
+                                                      expected.get(2),
+                                                      expected.get(3),
+                                                      expected.get(4));
+        assertEquals(expected, result);
+    }
+
+    @Test
+    final void of_array() {
+        final String[] elements = Stream.generate(GENERATOR::anyString).limit(4).toArray(String[]::new);
+        final List<String> expected = List.of(elements);
+        final FinalList<String> result = FinalList.of(elements);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    final void of_collection() {
+        final List<String> expected = Stream.generate(GENERATOR::anyString)
+                                            .limit(GENERATOR.anyInt(10))
+                                            .toList();
+        final FinalList<CharSequence> result = FinalList.of(expected);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    final void of_streamable() {
+        final List<String> expected = Stream.generate(GENERATOR::anyString)
+                                            .limit(GENERATOR.anyInt(10))
+                                            .toList();
+        final Streamable<String> stage = expected::stream;
+        final FinalList<CharSequence> result = FinalList.of(stage);
         assertEquals(expected, result);
     }
 
     @Test
     final void of_nullable() {
-        final List<Integer> source = Arrays.asList(1, 2, 3, null, 2, 3, 4, null, 3, 4, 5);
-        final FinalList<Comparable<?>> result = FinalList.of(source);
-        assertTrue(result.contains(null));
-        assertEquals(source, result);
+        final List<Integer> expected = Arrays.asList(1, 2, 3, null, 2, 3, 4, null, 3, 4, 5);
+        final FinalList<Number> result = FinalList.of(expected);
+        assertEquals(expected, result);
     }
 
     @Test
-    final void of() {
-        final List<Integer> expected = Stream.generate(() -> GENERATOR.anyInt(0, 10))
-                                             .limit(20)
-                                             .toList();
-
-        final FinalList<Number> result = FinalList.of(expected);
-        assertEquals(expected, result);
+    final void immutable() {
+        final FinalList<String> sample = FinalList.of("1", "2", "3");
+        assertThrows(UnsupportedOperationException.class, () -> sample.add("4"));
+        assertThrows(UnsupportedOperationException.class, () -> sample.remove(1));
+        assertThrows(UnsupportedOperationException.class, () -> sample.remove("2"));
+        assertThrows(UnsupportedOperationException.class, () -> {
+            final Iterator<String> iterator = sample.iterator();
+            iterator.next();
+            iterator.remove();
+        });
+        assertThrows(UnsupportedOperationException.class, () -> sample.sort(String::compareTo));
+        assertThrows(UnsupportedOperationException.class, sample::clear);
     }
 }
--- patterns-lib/zz-collection-mneme/src/test/java/de/team33/patterns/collection/mneme/publics/FinalSetTest.java
+++ patterns-lib/zz-collection-mneme/src/test/java/de/team33/patterns/collection/mneme/publics/FinalSetTest.java
@@ -2,18 +2,14 @@ package de.team33.patterns.collection.mneme.publics;
 
 import de.team33.patterns.arbitrary.mimas.Generator;
 import de.team33.patterns.collection.mneme.FinalSet;
-import de.team33.patterns.streamable.naiad.Streamer;
+import de.team33.patterns.streamable.naiad.Streamable;
 import org.junit.jupiter.api.Test;
 
 import java.security.SecureRandom;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class FinalSetTest {
 
@@ -21,50 +17,92 @@ class FinalSetTest {
 
     @Test
     final void empty() {
-        assertTrue(FinalSet.empty().isEmpty());
+        assertEquals(Set.of(), FinalSet.empty());
     }
 
     @Test
-    final void tryBuild_map_reduce() {
-        final List<Integer> source = Stream.generate(() -> GENERATOR.anyInt(0, 10))
-                                           .limit(20000)
-                                           .toList();
-        final Set<Integer> expected = new LinkedHashSet<>(source);
-        final FinalSet<Integer> result = source.stream()
-                                               .map(Streamer::of)
-                                               .reduce(Streamer.empty(), Streamer::addAll)
-                                               .map(FinalSet::of);
-        assertEquals(List.copyOf(expected), List.copyOf(result));
+    final void of_single() {
+        final List<String> expected = Stream.generate(GENERATOR::anyString).limit(1).toList();
+        final FinalSet<String> result = FinalSet.of(expected.get(0));
+        assertEquals(expected, result.stream().toList());
     }
 
     @Test
-    final void tryBuild_collect() {
-        final List<Integer> source = Stream.generate(() -> GENERATOR.anyInt(0, 10))
-                                           .limit(20000)
-                                           .toList();
-        final Set<Integer> expected = new LinkedHashSet<>(source);
-        final FinalSet<Integer> result = source.stream()
-                                               .collect(Streamer::<Integer>empty, Streamer::add, Streamer::addAll)
-                                               .map(FinalSet::of);
-        assertEquals(List.copyOf(expected), List.copyOf(result));
+    final void of_two() {
+        final List<String> expected = Stream.generate(GENERATOR::anyString).distinct().limit(2).toList();
+        final FinalSet<String> result = FinalSet.of(expected.get(0), expected.get(1));
+        assertEquals(expected, result.stream().toList());
     }
 
     @Test
-    final void of_nullable() {
-        final List<Integer> source = Arrays.asList(1, 2, 3, null, 2, 3, 4, null, 3, 4, 5);
-        final FinalSet<Comparable<?>> result = FinalSet.of(source);
+    final void of_three() {
+        final List<String> expected = Stream.generate(GENERATOR::anyString).distinct().limit(3).toList();
+        final FinalSet<String> result = FinalSet.of(expected.get(0), expected.get(1), expected.get(2));
+        assertEquals(expected, result.stream().toList());
+    }
+
+    @Test
+    final void of_more() {
+        final List<String> expected = Stream.generate(GENERATOR::anyString).distinct().limit(5).toList();
+        final FinalSet<String> result = FinalSet.of(expected.get(0),
+                                                    expected.get(1),
+                                                    expected.get(2),
+                                                    expected.get(3),
+                                                    expected.get(4));
+        assertEquals(expected, result.stream().toList());
+    }
+
+    @Test
+    final void of_array() {
+        final String[] elements = Stream.generate(GENERATOR::anyString)
+                                        .distinct()
+                                        .limit(GENERATOR.anyInt(10))
+                                        .toArray(String[]::new);
+        final List<String> expected = List.of(elements);
+        final FinalSet<String> result = FinalSet.of(elements);
+        assertEquals(expected, result.stream().toList());
+    }
+
+    @Test
+    final void of_collection() {
+        final List<String> expected = Stream.generate(GENERATOR::anyString)
+                                            .distinct()
+                                            .limit(GENERATOR.anyInt(10))
+                                            .toList();
+        final FinalSet<CharSequence> result = FinalSet.of(expected);
+        assertEquals(expected, result.stream().toList());
+    }
+
+    @Test
+    final void of_streamable() {
+        final List<String> expected = Stream.generate(GENERATOR::anyString)
+                                            .distinct()
+                                            .limit(GENERATOR.anyInt(10))
+                                            .toList();
+        final Streamable<String> stage = expected::stream;
+        final FinalSet<CharSequence> result = FinalSet.of(stage);
+        assertEquals(expected, result.stream().toList());
+    }
+
+    @Test
+    final void of_nullable_distinct() {
+        final List<Integer> origin = Arrays.asList(1, 2, 3, null, 2, 3, 4, null, 3, 4, 5);
+        final Set<Integer> expected = new LinkedHashSet<>(origin);
+        final FinalSet<Number> result = FinalSet.of(origin);
+        assertEquals(expected, result);
         assertTrue(result.contains(null));
     }
 
     @Test
-    final void of() {
-        final List<Integer> source = Stream.generate(() -> GENERATOR.anyInt(0, 10))
-                                           .limit(20)
-                                           .toList();
-        final Set<Integer> expected = new LinkedHashSet<>(source);
-        assert expected.size() <= 10 : "Expected max. 10 elements - but was %d".formatted(expected.size());
-
-        final FinalSet<Number> result = FinalSet.of(source);
-        assertEquals(List.copyOf(expected), List.copyOf(result));
+    final void immutable() {
+        final FinalSet<String> sample = FinalSet.of("1", "2", "3");
+        assertThrows(UnsupportedOperationException.class, () -> sample.add("4"));
+        assertThrows(UnsupportedOperationException.class, () -> sample.remove("2"));
+        assertThrows(UnsupportedOperationException.class, () -> {
+            final Iterator<String> iterator = sample.iterator();
+            iterator.next();
+            iterator.remove();
+        });
+        assertThrows(UnsupportedOperationException.class, sample::clear);
     }
 }
