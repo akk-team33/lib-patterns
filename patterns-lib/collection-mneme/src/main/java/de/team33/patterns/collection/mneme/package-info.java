dc2524620c4a4ca6531bfec2811e90753b7539e9
Andi Kluge <akk-dev@team33.de>
2026-09-11 14:38:23 +0200

    [2.x] collection-mneme: added package-info

---
+++ patterns-lib/collection-mneme/src/main/java/de/team33/patterns/collection/mneme/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides immutable collection types that preserve the encounter order of their source
+ * and may contain {@code null} elements.
+ *
+ * @see <a href="https://de.wikipedia.org/wiki/Mneme_(Mond)" target="_blank">Mneme (Mond)</a>
+ * @see <a href="https://de.wikipedia.org/wiki/Mneme" target="_blank">Mneme (Mythologie)</a>
+ */
+package de.team33.patterns.collection.mneme;
