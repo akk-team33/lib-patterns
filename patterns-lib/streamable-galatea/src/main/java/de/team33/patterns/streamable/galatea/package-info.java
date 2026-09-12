6731b7a144b8ef27d31a233881bb42b9759774fc
Andi Kluge <akk-dev@team33.de>
2026-09-12 04:09:12 +0200

    [2.x] streamable-galatea: refined module description

--- patterns-lib/streamable-galatea/src/main/java/de/team33/patterns/streamable/galatea/package-info.java
+++ patterns-lib/streamable-galatea/src/main/java/de/team33/patterns/streamable/galatea/package-info.java
@@ -1,6 +1,7 @@
 /**
- * Provides streamable classes.
+ * Provides a common abstraction for streamable types.
  *
- * @see <a href="https://de.wikipedia.org/wiki/Galatea_(Mond)">Galatea (Mond)</a>
+ * @see <a href="https://de.wikipedia.org/wiki/Galatea_(Mond)" target="_blank">Galatea (Mond)</a>
+ * @see <a href="https://de.wikipedia.org/wiki/Galateia" target="_blank">Galateia (Mythologie)</a>
  */
 package de.team33.patterns.streamable.galatea;
