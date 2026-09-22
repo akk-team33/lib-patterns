a9cf3637a660df005fc701245cd20467f76d4df6
Andi Kluge <akk-dev@team33.de>
2026-09-22 11:27:12 +0200

    [2.x] zz-arbitrary-mimas: reviewing Charger

--- patterns-lib/zz-arbitrary-mimas/src/main/java/de/team33/patterns/arbitrary/mimas/Charger.java
+++ patterns-lib/zz-arbitrary-mimas/src/main/java/de/team33/patterns/arbitrary/mimas/Charger.java
@@ -4,25 +4,26 @@ import java.util.List;
 
 /**
  * A utility interface:
- * can extend producer classes with the functionality to fill classic mutable Java data objects* with (typically)
- * random values.
+ * can extend producer classes with the functionality to fill <em>classic mutable data objects</em>* with (typically)
+ * arbitrary values.
  * <p>
  * In more general terms, all target instances can be filled with values via reflection if they have suitable
- * methods for receiving these values: so-called setters**.
- * Within certain limits, this can also apply, for example, to target instances according to the builder pattern.
+ * methods for receiving these values: so-called <em>setters</em>**.
+ * Within certain limits, this can also apply to target instances according to the builder pattern.
  * <p>
- * The producer class that implements this interface must provide appropriate getters*** to supply the values
- * that can be passed to the target instance's setters.
+ * The producer class itself, which implements this interface, must provide suitable <em>getters</em>*** to supply
+ * the values that can be passed to the setters of the target instance.
  * <p>
- * Static, synthetic and native methods are generally ignored.
+ * Static, synthetic and native methods, as well as those defined by the class {@link Object}, are generally ignored.
  * <p>
- * *<em>Classic Java data objects</em> in this context means objects that are (essentially) made up of properties
- * and whose property values can be determined or set using so-called getters and setters**.
+ * *<b><em>Classic mutable data objects</em></b> in this context means objects that are (essentially) made up of
+ * properties and whose property values can be determined or updated using so-called <em>getters</em> and
+ * <em>setters</em>**.
  * <p>
- * **<em>Setters</em> in this context are public instance methods that take exactly one argument as a parameter
+ * **<b><em>Setters</em></b> in this context are public instance methods that take exactly one argument as a parameter
  * and return nothing (void) or, according to the builder pattern, the target instance itself as the result.
  * <p>
- * ***<em>Getters</em> in this context are public instance methods that take no parameters and return a result
+ * ***<b><em>Getters</em></b> in this context are public instance methods that take no parameters and return a result
  * of a specific type (not void). The {@link Object#hashCode() hashCode()} and {@link Object#toString() toString()}
  * methods are ignored.
  *
@@ -31,8 +32,8 @@ import java.util.List;
 public interface Charger {
 
     /**
-     * Initializes the properties of a given {@code target} instance with values provided by <em>this</em> Charger instance
-     * itself and returns the initialized target.
+     * Initializes the properties of a given {@code target} instance with values provided by <em>this</em> Charger
+     * instance itself and returns the initialized target.
      * <p>
      * In order for a property to be initialized, the {@code target}'s class must provide a corresponding setter
      * that is actually accessible from <em>this</em>. In addition, <em>this</em> Charger's implementation class
@@ -42,7 +43,7 @@ public interface Charger {
      * It makes little sense to override <em>this</em> method or then even use <em>this</em> interface at all.
      *
      * @param target The target of the operation.
-     * @param ignore The names of methods that shell be ignored.
+     * @param ignore The names of setters that shell be ignored.
      * @param <T>    The target type.
      * @return The target.
      */
